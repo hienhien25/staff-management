87ed7523e9bc336160ac494a6e64e17223aabaf7
Hien Hien <hienhien2511997@gmail.com>
2019-04-05 17:34:14 +0700

    use ajax to checkin

--- app/Http/Controllers/CheckinController.php
+++ app/Http/Controllers/CheckinController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Checkin;
+use Auth;
 class CheckinController extends Controller
 {
     public function getcheckin()
@@ -15,6 +16,7 @@ class CheckinController extends Controller
     public function postcheckin(Request $req)
     {
     	$check= new Checkin;
+    	$check->id_staff=Auth::user()->id;
     	$check->start_hour=$req->start_hour;
     	$check->finish_hour=$req->finish_hour;	
     	$check->check_date=date('m-d-Y');
