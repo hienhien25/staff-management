47c728dd5195e4eaf89bd33c0174a48458a28af9
Hien Hien <hienhien2511997@gmail.com>
2019-04-13 09:38:48 +0700

    Modify the department code according to PSR-2

---
+++ app/Department.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    protected $table='tbldepartment';
+    protected $fillable=['department_name','quantity'];
+    public function relaStaff()
+    {
+    	return $this->hasMany('App\User','id_department');
+    }
+    public function relaPosition()
+    {
+    	return $this->hasMany('App\Position','id_department');
+    }
+}
--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -5,38 +5,50 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Department;
 use App\Position;
+use App\User;
+use DB;
+use App\Http\Requests\SaveDepartmentRequest;
 
 class DepartmentController extends Controller
 {
-    public function showlist()
+    public function showList()
     {
     	$dep=Department::paginate(12);
+        $sl=DB::table('users')->where('id_department','=',1)->get();
+        $qtt=count($sl);
+        //dd($qtt);
     	return view('layout.department.department_list',compact('dep'));
     }
-    public function getadd()
+    public function getAdd()
     {
     	return view('layout.department.addDepartment');
     }
-    public function postadd(Request $req)
+    public function postAdd(SaveDepartmentRequest $req)
     {
-    	$this->validate($req,
-    		[
-    			'department_name'=>'required'
-    		],
-    		[
-    			'department_name.required'=>'Bạn chưa nhập tên phòng !'
-    		]
-    	);
-    	$de= new Department;
-    	$de->department_name=$req->department_name;
-    	$de->quantity=$req->quantity;
+    	$de= new Department();
+    	$de->fill($req->all());
     	$de->save();
-    	return redirect(route('admin.DepartmentList'));
+    	return redirect(route('admin.departmentList'));
     }
-    public function showlistposition($id)
+    public function showListPosition($id)
     {
-    	$sp=Position::where('id_department',$id)->paginate(12);
-        dd($sp);
-    	return view('layout.department.department_list',compact('sp'));
+     //    $dep=Department::find('id');
+    	// $sp=Position::where('id_department',$id)->paginate(12);
+        // $count=Position::where('id_department',$id)->count();
+        // dd($count);
+        //dd($sp);
+       $pos=DB::table('tbldepartment')
+                    ->leftjoin('tblposition', 'tblposition.id_department', '=', 'tbldepartment.id')
+                    ->where('id_department',$id)
+                    ->select('tbldepartment.*', 'tblposition.*', 'tbldepartment.department_name as department_name','tblposition.position_name as position_name','tblposition.description as description')
+                    ->paginate(12);
+    	return view('layout.position.list_position',compact('pos'));
+    }
+    public function getDelete($id)
+    {
+        $de=Department::find($id);
+        //dd($de);
+        $de->delete();
+        return redirect(route('admin.departmentList'));
     }
 }
