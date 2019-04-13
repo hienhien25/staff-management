250d6a03417eb25c4e8d26be2be8392882124be8
Hien Hien <hienhien2511997@gmail.com>
2019-04-13 10:14:57 +0700

    Modify the position  code according to PSR-2

--- app/Http/Controllers/PositionController.php
+++ app/Http/Controllers/PositionController.php
@@ -4,24 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Position;
-use App\StaffPosition;
-use App\Staff;
+use App\Department;
 use DB;
+use App\Http\Requests\SavePossitionRequest;
 class PositionController extends Controller
 {
-    public function showlist()
+    public function showStaffList($id)
     {
-    	$sl=Position::paginate(12);
-    	return view('layout.position.list_position',compact('sl'));
-    }
-    public function showstafflist($id)
-    {
-    	$sh=DB::table('tblstaff_position')
-                    ->join('tblstaff', 'tblstaff_position.id', '=', 'tblstaff.id_staffpos')
-                    ->where('id_position',$id)
-                    ->select('tblstaff_position.*', 'tblstaff.*')
+    	$sh=DB::table('tbldepartment')
+                    ->join('tblposition', 'tbldepartment.id', '=', 'tblposition.id_department')
+                    ->rightJoin('users','tbldepartment.id','=','users.id_department')
+                    ->where('users.id_department',$id)
+                    ->select('tbldepartment.*','tblposition.*','users.*','users.fullname as fullname','users.email as email','users.role as role')
                     ->paginate(12);
+        //dd($sh);
         return view('layout.position.show_list_staff',compact('sh'));
+    }
+    public function getDelete($id)
+    {
+        $dp=Position::find('id');
+        $dp->delete();
+        return redirect(route('admin.positionList'));
+    }
+    public function getAdd()
+    {
+        $de=Department::all();
+        return view('layout.position.add_position',compact('de'));
+    }
+    public function postAdd(SavePossitionRequest $req)
+    {
+        $posi=new Position();
+        $posi->fill($req->all());
+        $posi->id_department=$req->department;
+        $posi->save();
+        return redirect()->back()->with('msg','Bạn đã thêm thành công !');;
     }
    
 }
