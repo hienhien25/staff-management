0bd31fbf2e838d4f5664828d920fa66a5b7fa05e
Hien Hien <hienhien2511997@gmail.com>
2019-05-14 17:36:45 +0700

    edit function checkin, checkout

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -2,6 +2,7 @@
 $defaultImg = Auth::user()->image == null ? asset('images/avatarDefault.jpeg') : Auth::user()->image;
 @endphp
 <!-- Left side column. contains the logo and sidebar -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <aside class="left-side sidebar-offcanvas">                
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
@@ -46,32 +47,72 @@ $defaultImg = Auth::user()->image == null ? asset('images/avatarDefault.jpeg') :
             </li>
             <li >
                 <a href="{{route('admin.userList')}}">
-                   <i class="fa fa-th"></i> <span>User List</span> 
-               </a>
-           </li>
-           <li>
-            <a href="{{route('admin.sendMail')}}">
-                <i class="fa fa-envelope"></i> <span>Mail</span>
-                <small class="badge pull-right bg-yellow">12</small>
+                 <i class="fa fa-th"></i> <span>User List</span> 
+             </a>
+         </li>
+         @if(Auth::user()->role==1)
+         <li class="treeview">
+            <a href="#">
+                <i class="fa fa-laptop"></i>
+                <span>Statistics</span>
+                <i class="fa pull-right fa-angle-left"></i>
             </a>
+            <ul class="treeview-menu" style="display: none;">
+                <li><a href="{{route('CheckoutListPerMonth')}}" style="margin-left: 10px;"><i class="fa fa-angle-double-right"></i>All Staff List</a></li>
+                <li><a href="{{route('admin.statisticPersonal')}}" style="margin-left: 10px;"><i class="fa fa-angle-double-right"></i>Personal</a></li>
+
+            </ul>
         </li>
+        @elseif(Auth::user()->role==0)
         <li >
-            <a href="{{route('admin.log')}}">
-                <i class="fa fa-edit"></i> <span>Blog</span>
-            </a>
-        </li>
-        <li>
-           <div class="btn-group" style="width: 150px; align-items: center;">
-            <button type="button" class="btn btn-warning">Action</button>
-            <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown">
-                <span class="caret"></span>
-                <span class="sr-only">Toggle Dropdown</span>
-            </button>
-            <ul class="dropdown-menu" role="menu" style="background-color: #f39c12 ;">
-                <li><a href="{{route('admin.checkout')}}">Checkout</a></li>
-                <li><a class="btn btn-danger waves-effect waves-light remove-record" data-toggle="modal" data-url="{!! URL::route('admin.checkin') !!}" data-id="{{Auth::user()->id}}" data-target="#custom-width-modal">Checkin</a></li></li>
+            <a href="{{route('admin.statisticPersonal')}}">
+             <i class="fa fa-th"></i> <span>Statistics</span> 
+         </a>
+     </li>
+     @endif
+
+     <li>
+        <a href="{{route('admin.mailbox')}}">
+            <i class="fa fa-envelope"></i> <span>Mail</span>
+            <!-- <small class="badge pull-right bg-yellow">12</small> -->
+        </a>
+    </li>
+    <li >
+        <a href="{{route('admin.log')}}">
+            <i class="fa fa-edit"></i> <span>Blog</span>
+        </a>
+    </li>
+    <li>
+     <div class="btn-group" style="width: 150px; align-items: center;">
+        <button type="button" class="btn btn-warning">Action</button>
+        <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown">
+            <span class="caret"></span>
+            <span class="sr-only">Toggle Dropdown</span>
+        </button>
+        <ul class="dropdown-menu" role="menu" style="background-color: #f39c12 ;">
+            <?php
+            $date=date('Y-m-d');
+            $check=DB::table('tblcheckin')
+            ->join('tblstatistic','tblcheckin.id_statist','=','tblstatistic.id')
+            ->where('tblstatistic.id_staff',Auth::user()->id)
+            ->where('tblcheckin.check_date',$date)
+            ->first();
+            if(!isset($check))
+            {
+                ?>
+                <li><a   data-url="{!! URL::route('admin.checkin') !!}" data-id="{{Auth::user()->id}}" id="btncheckin" class="btncheckin
+                    ">Checkin</a></li>
+                    <?php
+                }elseif(isset($check)&&$check->status==0){
+                    ?>
+                    <li><a  data-url="{!! URL::route('admin.checkout') !!}" data-id="{{Auth::user()->id}}" id="btncheckout" class="btncheckout">Checkout</a></li>
+                    <?php
+                }
+                ?>
+
+
                 @if(Auth::user()->role==1)
-                <li><a href="{{route('admin.addMember')}}">Add new Member</a></li>
+                <li><a  data-target="#addMember" data-toggle="modal" >Add new Member</a></li>
                 @endif
             </ul>
         </div>
@@ -81,7 +122,7 @@ $defaultImg = Auth::user()->image == null ? asset('images/avatarDefault.jpeg') :
 <!-- /.sidebar -->
 </aside>
 <!-- checkin -->
-<form action="{{route('admin.checkin')}}"  class="remove-record-model" method="post">
+<!-- <form action="{{route('admin.checkin')}}"  class="remove-record-model" method="post">
     @csrf
     <div id="custom-width-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="custom-width-modalLabel" aria-hidden="true" style="display: none;">
         <div class="modal-dialog">
@@ -106,38 +147,112 @@ $defaultImg = Auth::user()->image == null ? asset('images/avatarDefault.jpeg') :
 
             </div>
             <div class="modal-footer clearfix">
-
-                <button type="button" class="btn btn-danger" data-dismiss="modal" onclick="location.replace('http://staff-manage.local/admin');" ></i>Cancel</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal"  ></i>Cancel</button>
 
                 <button type="submit" class="btn btn-primary pull-left" id="checkin"></i> OK</button>
             </div>
         </div>
     </div>
 </div>
-</form>
-<!-- Checkout -->
-@yield('checkout')
+</form> -->
+<!-- Add member -->
+<div class="modal fade" id="addMember" tabindex="-1" role="dialog" aria-hidden="true" >
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true" >×</button>
+                <h4 class="modal-title"><i class="glyphicon glyphicon-user"></i> Add member </h4>
+            </div>
+            <form action="{{route('admin.addMember')}}" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="modal-body">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <span class="input-group-addon">Email:</span>
+                            <input name="email" id="email" type="text" class="form-control" placeholder="Email*">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer clearfix">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal"  >Cancel</button>
+                    <button type="submit" class="btn btn-primary pull-left" onclick="alert('Are you sure ?');"> OK</button>
+                </div>
+            </form>
+        </div><!-- /.modal-content -->
+    </div>
+</div>
 @section('pagejs')
 <script type="text/javascript">
     $(document).ready(function(){
-        $('.remove-record').click(function() {
-            var id = $(this).attr('data-id');
-            var url = $(this).attr('data-url');
-            var token = CSRF_TOKEN;
-            $(".remove-record-model").attr("action",url);
-            $('#checkin').click(function(){
-                var start_h=$("#start_hour").val();
-                var finish_h=$("#finish_hour").val();
-            //alert(start_h);
-            $.ajax({
-                type : "POST",
-                url:"<?php echo url('/admin/checkin'); ?>", 
-                data:{start_hour:start_h,finish_hour:finish_h}
-                success: function(){
-                    alert('ok');
-                }
-            });
+        $('.btncheckin').click(function(){
+            var id=$(this).attr('data-id');
+            var start_hour=moment().format('HH:mm');
+            swal({
+              title: "Are you sure?",
+              type: "warning",
+              showCancelButton: true,
+              confirmButtonClass: "btn-danger",
+              confirmButtonText: "Yes",
+              cancelButtonText: "Cancel",
+              closeOnConfirm: false,
+              closeOnCancel: false
+          },
+          function(isConfirm) {
+              if (isConfirm) {
+                //alert('success');
+                 //swal("Checkin!", "Your imaginary file has been deleted.", "success");
+                $.ajax({
+                    type:'POST',
+                    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                    url:"<?php echo url('/admin/post-checkin'); ?>",
+                    data:{'id':id,'start_hour':start_hour},
+                    success:function(data){
+                        if(data['success']){
+                            swal("Checkin!", "Successfully!", "success");
+                        }
+                    }
+                });
+                
+            } else {
+                swal("Cancelled", "Your imaginary file is safe :)", "error");
+            }
+        });
         });
     });
+    //Checkout
+    $('.btncheckout').click(function(){
+        var finish_hour=moment().format('HH:mm');
+        swal({
+              title: "Are you sure?",
+              type: "warning",
+              showCancelButton: true,
+              confirmButtonClass: "btn-danger",
+              confirmButtonText: "Yes",
+              cancelButtonText: "Cancel",
+              closeOnConfirm: false,
+              closeOnCancel: false
+          },
+          function(isConfirm) {
+              if (isConfirm) {
+                //alert('success');
+                 //swal("Checkin!", "Your imaginary file has been deleted.", "success");
+                $.ajax({
+                    type:'POST',
+                    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                    url:"<?php echo url('/admin/post-checkout'); ?>",
+                    data:{'finish_hour':finish_hour},
+                    success:function(data){
+                        if(data['success']){
+                            swal("Checkout!", "Successfully!", "success");
+                        }
+                    }
+                });
+                
+            } else {
+                swal("Cancelled", "Your imaginary file is safe :)", "error");
+            }
+        });
+    });
+
 </script>
 @endsection
